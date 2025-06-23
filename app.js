0108f4b28dfb855685b66b52cf6bff5fac38d5dc
InnaLiu07 <byyf210918@gmail.com>
2025-06-23 13:37:57 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+const cors = require("cors");
 const express = require("express");
 const app = express();
 
 const apiRouter = require("./routes/api.router.js");
+
+app.use(cors());
 
 app.use(express.json());
 app.use("/api", apiRouter);
